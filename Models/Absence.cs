c7c48d19be55b14769bd3c94eeaedd1e0e2f76c8
goodjonte <goodjonte@gmail.com>
2023-06-20 16:29:00 +1200

    Added AttendaceId property to absence class

--- Models/Absence.cs
+++ Models/Absence.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public Guid StudentId { get; set; }
         public Guid ClassId { get; set; }
+        public Guid AttendanceId { get; set; }
 
     }
 }
